c487e044e978efda52dc6496ed6d5a2f0937fdcc
Melvyn Herzig <melvyn.herzig@heig-vd.ch>
2022-07-15 10:04:10 +0200

    AbstractHtmlExport polished

--- dev/tucuxi-tuberxpert/src/tuberxpert/exporter/abstracthtmlexport.h
+++ dev/tucuxi-tuberxpert/src/tuberxpert/exporter/abstracthtmlexport.h
@@ -8,6 +8,8 @@
 namespace Tucuxi {
 namespace Xpert {
 
+/// \brief Class that must be implemented by any html exporter that
+///        want to be used by the pdf exporter.
 /// \date 23/06/2022
 /// \author Herzig Melvyn
 class AbstractHtmlExport
@@ -17,6 +19,9 @@ public:
     /// \brief Destructor.
     virtual ~AbstractHtmlExport() {};
 
+    /// \brief Export the given XpertRequestResult to an html file named with the given file name.
+    /// \param _fileName Name of the file to use for the html file.
+    /// \param _xpertRequestResult Result of the xpertRequest to export.
     virtual void exportToFile(const std::string& _fileName, XpertRequestResult& _xpertRequestResult) = 0;
 };
 
